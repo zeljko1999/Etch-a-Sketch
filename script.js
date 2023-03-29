d5dcac9166331aca1d11750ba932754296645717
zeljko1999 <zeljkoculibrk7@gmail.com>
2023-03-29 14:54:49 +0200

    Add reset button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".flex-container");
+
 function createGrid(x,y){
     for(let i=0;i < x; i++)
     {
@@ -13,14 +14,31 @@ function createGrid(x,y){
             newRow.appendChild(newDiv);
             }
     }
-}
-createGrid(16,16);
-let inputs = document.getElementsByClassName("column");
-for(let i=0; i <inputs.length ; i++)
+    let inputs = document.getElementsByClassName("column");
+    for(let i=0; i <inputs.length ; i++)
     inputs[i].addEventListener("mouseover", function() {
-        inputs[i].classList.add("change-color");
+        inputs[i].style.backgroundColor = "black";
     })
 
+}
+createGrid(16,16);
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", function() {
+    let newRow=0;
+    let newColumn=0;
+    do{
+    newRow= prompt("How many rows do you want? Max 100.");
+    if(newRow > 100 || newRow < 0 )
+        alert("Invalid value.");
+    } while(newRow>100 || newRow<0);
+    do{
+    newColumn=prompt("How many columns do you want? Max 100"); 
+    if(newColumn > 100 || newColumn < 0 )
+        alert("Invalid value.");
+    } while(newColumn> 100 || newColumn < 0 );
+    container.replaceChildren();
+    createGrid(newRow,newColumn);
+});
 
 
 
